8e1dd31822f66cd5a9e4ed3c5942d5ae4136a22f
Scott Hoecker <hoeckers@gmail.com>
2022-02-24 10:28:43 -0600

    Finish up medium exercises

--- exercises/medium-1/6_method_to_proc.rb
+++ exercises/medium-1/6_method_to_proc.rb
@@ -48,16 +48,19 @@ section for this exercise; it should prove useful.
 =end
 
 def convert_to_base_8(n)
-  n.method_name.method_name # replace these two method calls
+  # n.method_name.method_name # replace these two method calls
+  n.to_s(8).to_i
 end
 
 # The correct type of argument must be used below
-base8_proc = method(argument).to_proc
+# base8_proc = method(argument).to_proc
+base8_proc = method(:convert_to_base_8).to_proc
 
 # We'll need a Proc object to make this code work. Replace `a_proc`
 # with the correct object
-[8, 10, 12, 14, 16, 33].map(&a_proc)
+# [8, 10, 12, 14, 16, 33].map(&a_proc)
+result = [8, 10, 12, 14, 16, 33].map(&base8_proc)
 
 # The expected return value of map on this number array should be:
 
-[10, 12, 14, 16, 20, 41]
+p result == [10, 12, 14, 16, 20, 41]
